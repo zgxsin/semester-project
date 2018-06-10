bae7a264f4966c7269bf9304b2e1c458252258c0
Hari <g.x.zhou@outlook.com>
2018-06-10 11:52:20 +0200

    modify function 'load_image' to get the image from both datasets

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -20,6 +20,7 @@ import skimage.transform
 import urllib.request
 import shutil
 import warnings
+import PIL
 
 # URL from which to download the latest COCO trained weights
 COCO_MODEL_URL = "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
@@ -361,7 +362,13 @@ class Dataset(object):
         """Load the specified image and return a [H,W,3] Numpy array.
         """
         # Load image
-        image = skimage.io.imread(self.image_info[image_id]['path'])
+        # if path object is ndarray, just read it directly
+        if isinstance(self.image_info[image_id]['path'], np.ndarray):
+            image = self.image_info[image_id]['path']
+
+        else:
+            image = skimage.io.imread(self.image_info[image_id]['path'])
+
         # If grayscale. Convert to RGB for consistency.
         if image.ndim != 3:
             image = skimage.color.gray2rgb(image)
@@ -497,7 +504,7 @@ def resize_image(image, min_dim=None, max_dim=None, min_scale=None, mode="square
     return image.astype(image_dtype), window, scale, padding, crop
 
 
-def resize_mask(mask, scale, padding, crop=None):
+def resize_maskmask(mask, scale, padding, crop=None):
     """Resizes a mask using the given scale and padding.
     Typically, you get the scale and padding from resize_image() to
     ensure both, the image and the mask, are resized consistently.
